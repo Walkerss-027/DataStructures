0507d28f77dd67f398b50169fac24fe8b4fec7b7
Autimaticcc <whutwangjingg@gmail.com>
2021-10-04 18:48:58 +0800

    10.4

--- src/com/atguigu/Stack/Caculator.java
+++ src/com/atguigu/Stack/Caculator.java
@@ -4,45 +4,49 @@ public class Caculator {
 
     public static void main(String[] args) {
 
+        String experssion="3+2*6-2";
+        ArrayStack2 numStack = new ArrayStack2(10);
+        ArrayStack2 operStack = new ArrayStack2(10);
+
     }
 
 }
 
-class ArrayStack2{
+class ArrayStack2 {
     private int maxSize;    //栈的大小
     private int[] stack;    //数组模拟栈
-    private int top=-1;     //top表示栈顶，初始化为-1
+    private int top = -1;     //top表示栈顶，初始化为-1
 
     //构造器
-    public ArrayStack2(int maxSize){
-        this.maxSize=maxSize;
-        stack=new int[this.maxSize];
+    public ArrayStack2(int maxSize) {
+        this.maxSize = maxSize;
+        stack = new int[this.maxSize];
     }
 
     //判断栈满
-    public boolean isFull(){
-        return top==maxSize-1;
+    public boolean isFull() {
+        return top == maxSize - 1;
     }
 
     //栈空
-    public boolean isEmpty(){
-        return top==-1;
+    public boolean isEmpty() {
+        return top == -1;
     }
 
     //入栈-push
-    public void push(int value){
+    public void push(int value) {
         //先判断是否为满
         if (isFull()) {
             System.out.println("栈满");
             return;
         }
         top++;
-        stack[top]=value;
+        stack[top] = value;
     }
 
     //出栈-pop,返回栈顶元素
-    public int pop(){
-        if (isEmpty()){ //判断是否为空
+    public int pop() {
+        if (isEmpty()) { //判断是否为空
             throw new RuntimeException("栈空，没有数据");  //抛出异常
         }
         int value = stack[top];
@@ -51,14 +55,52 @@ class ArrayStack2{
     }
 
     //显示栈的情况[遍历栈]，遍历时，需要从栈顶开始显示数据
-    public void list(){
-        if (isEmpty()){
+    public void list() {
+        if (isEmpty()) {
             System.out.println("栈空，没有数据~");
         }
         for (int i = top; i >= 0; i--) {
-            System.out.printf("stack[%d]=%d\n",i,stack[i]);
+            System.out.printf("stack[%d]=%d\n", i, stack[i]);
 
         }
+    }
+
+    //返回运算符的优先级，优先级使用数字表示，数字越大优先级越高
+    public int priority(int oper) {
+        if (oper == '*' || oper == '/') {
+            return 1;
+        } else if (oper == '+' || oper == '-') {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+
+    //判断是不是一个运算符
+    public boolean isOper(char val) {
+        return val == '+' || val == '-' || val == '*' || val == '/';
+    }
+
+
+    //计算方法
+    public int cal(int num1,int num2,int oper){
+        int res=0;  //res用于存放计算的结果
+        switch (oper){
+            case '+':
+                res=num1+num2;
+                break;
+            case '-':
+                res=num2-num1;
+                break;
+            case '*':
+                res=num1*num2;
+                break;
+            case '/':
+                res=num2/num1;
+                break;
+        }
+        return res;
     }
 
 
