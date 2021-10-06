0b332b7312058e52cb1e219165e85e785d76b8c8
Autimaticcc <whutwangjingg@gmail.com>
2021-10-06 10:06:24 +0800

    10.5

--- src/com/atguigu/Stack/PolandNotation.java
+++ src/com/atguigu/Stack/PolandNotation.java
@@ -17,9 +17,13 @@ public class PolandNotation {
         //3. 将得到的中缀表达式对应的 List => 后缀表达式对应的 List
         //      即 ArrayList [1,+,(,(,2,+,3,)4,),-,5] => ArrayList [1,2,3,+,4,*,+,5,-]
 
-        String expression = "1+((2+3)x4)-5";
-        List<String> infixExpressionList =toInfixExpressionList(expression);
-        System.out.println(infixExpressionList);    // [1, +, (, (, 2, +, 3, ), x, 4, ), -, 5]
+        String expression = "1+((2+3)*4)-5";
+        List<String> infixExpressionList = toInfixExpressionList(expression);
+        System.out.println("中缀表达式对应的List="+infixExpressionList);    // [1, +, (, (, 2, +, 3, ), x, 4, ), -, 5]
+        List<String > suffixExperssionList=parseSuffixExpressionList(infixExpressionList);
+        System.out.println("后缀表达式对应的List"+suffixExperssionList);    // [1,2,3,+,4,*,+,5,-]
+
+        System.out.printf("expression=%d",calculate(suffixExperssionList));
 
         /*
         //先定义逆波兰表达式
@@ -38,15 +42,8 @@ public class PolandNotation {
     }
 
 
-    //即 ArrayList [1,+,(,(,2,+,3,)4,),-,5] => ArrayList [1,2,3,+,4,*,+,5,-]
-    //方法：将得到的中缀表达式对应的List =>后缀表达式对应的List
-    public static List<String > parseSuffixExpressionList(List<String> ls){
-        //创建两个栈
-        Stack<String> s1 =
-    }
-
     //方法：将 中缀表达式转换成对应的List
-    //s="1+((2+3)x4)-5"
+    //s="1+((2+3)x4)-5" => ArrayList [1,+,(,(,2,+,3,),*,4,),-,5]
     public static List<String> toInfixExpressionList(String s) {
         //定义一个List，存放中缀表达式对应的内容
         List<String> ls = new ArrayList<String>();
@@ -71,6 +68,51 @@ public class PolandNotation {
         return ls;
     }
 
+
+    //即 ArrayList [1,+,(,(,2,+,3,),*,4,),-,5] => ArrayList [1,2,3,+,4,*,+,5,-]
+    //方法：将得到的中缀表达式对应的List =>后缀表达式对应的List
+    public static List<String> parseSuffixExpressionList(List<String> ls) {
+        //创建两个栈
+        Stack<String> s1 = new Stack<String>();
+        //说明：因为s2这个栈，再整个转换过程中，没有pop操作，而且后面我们还需要逆序输出
+        //因此比较麻烦，这里我们就不用Stack<String> 直接使用List<String> s2
+        //Stack<String> s2 = new Stack<String>();   //存储中间结果的栈 s2
+        List<String> s2 = new ArrayList<>();
+
+        //遍历ls
+        for (String item:ls){
+            //如果一个数，加入s2
+            if (item.matches("\\d+")){
+                s2.add(item);
+            }else if (item.equals("(")){
+                s1.push(item);
+            }else if (item.equals(")")){
+                //如果是右括号“）”，则依次弹出s1栈顶的运算符，并压入s2，直到遇到左括号为止，此时将这一对括号丢弃
+                while (!s1.peek().equals("(")){
+                    s2.add(s1.pop());
+                }
+                s1.pop();   //！！！ 将（弹出s1栈，消除小括号
+            } else {
+                //当item的优先级小于等于s1栈顶运算符，将s1栈顶的元素安抚弹出并加入到s2中，与s1中新的栈顶运算符相比较
+                //问题：缺少一个比较优先级高低的方法
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) >= Operation.getValue(item)){
+                    s2.add(s1.pop());
+                }
+                //还需要将item压入栈
+                s1.push(item);
+            }
+        }
+
+        //将s1中剩余的运算符依次弹出并加入s2
+        while (s1.size() != 0){
+            s2.add(s1.pop());
+        }
+
+        return s2;  //因为是存放到List中，因此按顺序输出就是对应的后缀表达式
+
+    }
+
+
     //将一个逆波兰表达式，依次将数据和运算符放入到ArrayList中
     public static List<String> getListString(String suffixExpression) {
         //将suffixExpression 分割
@@ -81,6 +123,7 @@ public class PolandNotation {
         }
         return list;
     }
+
 
     //完成对逆波兰表达式的运算
     /*
@@ -110,7 +153,7 @@ public class PolandNotation {
                     res = num1 + num2;
                 } else if (item.equals("-")) {
                     res = num1 - num2;
-                } else if (item.equals("x")) {
+                } else if (item.equals("*")) {
                     res = num1 * num2;
                 } else if (item.equals("/")) {
                     res = num1 / num2;
@@ -123,5 +166,37 @@ public class PolandNotation {
         }
         //最后留在stack中的数据就是运算结果
         return Integer.parseInt(stack.pop());
+    }
+}
+
+
+//编写一个类 Operation 可以返回一个运算符 对应的优先级
+class Operation{
+    private static int ADD=1;
+    private static int SUB=1;
+    private static int MUL=2;
+    private static int DIV=2;
+
+    //写一个方法，返回对应的优先级数字
+    public static int getValue(String operation){
+        int result = 0;
+        switch (operation){
+            case "+":
+                result=ADD;
+                break;
+            case "-":
+                result=SUB;
+                break;
+            case "*":
+                result=MUL;
+                break;
+            case "/":
+                result=DIV;
+                break;
+            default:
+                System.out.println("不存在该运算符");
+                break;
+        }
+        return result;
     }
 }
